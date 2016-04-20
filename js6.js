ad384b010656039142c8b80f66caf574db7c45b1
Yanxiong <592917974@qq.com>
2016-04-20 17:06:31 +0800

    原型模式

--- js6.js
+++ js6.js
@@ -106,3 +106,145 @@ function sayName(){
 	console.log(this.name);
 }
 
+
+
+//创建对象，时工厂模式
+function createPerson(name,age,job){
+	var o = new Object();
+	o.name = name;
+	o.age = age;
+	o.job = job;
+	o.sayName = function(){
+		console.log(this.name);
+	}
+	return o;
+}
+
+person1 = createPerson("Haha",20,"coder");
+console.log(person1);
+console.log(typeof(person1));
+console.log(person1 instanceof Object);
+
+
+//创建对象，原型模式
+function Person2(){
+}
+
+Person2.prototype.name = "Nicholas";
+Person2.prototype.age = 29;
+Person2.prototype.job = "Software Engineer";
+Person2.prototype.sayName = function(){
+	console.log(this.name);
+};
+
+person1 = new Person2();
+person1.sayName();
+console.log(person1);
+console.log(Person2.prototype);
+
+console.log(Person2.prototype.isPrototypeOf(person1));	//
+console.log(Object.getPrototypeOf(person1) == Person2.prototype);		//Object.getPrototypeOf(),返回[[Prototype]]的值
+
+person1.name = "haha";
+console.log(person1.name);
+
+console.log(person1.hasOwnProperty("name"));		//使用hasOwnProperty()方法可以检测一个属性是否存在于实例中
+
+delete person1.name;		//使用delete操作符删除实例属性
+console.log(person1.name);
+
+console.log(person1.hasOwnProperty("name"));
+
+
+console.log("name" in person1);	//in操作符会在通过对象能够访问给定属性时返回true
+
+function hasPrototypeProperty(object,name){
+	return !object.hasOwnProperty(name) && (name in object);
+}
+
+console.log(hasPrototypeProperty(person1,"name"));
+
+person1.name = "haha";
+console.log(hasPrototypeProperty(person1,"name"));
+
+console.log(Object.keys(Person2.prototype));
+console.log(Object.getOwnPropertyNames(Person2.prototype));
+
+function Person3(){}
+
+//使用对象字面量形式重写原型对象，construct属性指向Object，需要特意将其设置回适当的值，重设后会导致
+//[[Enumerable]]特性被设置为true
+Person3.prototype = {
+	constructor : Person3,
+	name : "Peter",
+	age : 24,
+	job : "Software Engineer",
+	sayName : function(){
+		console.log(this.name);
+	}
+};
+console.log(Object.keys(Person3.prototype));
+
+var friend = new Person3();
+console.log(friend instanceof Object);
+console.log(friend instanceof Person3);
+console.log(friend.constructor);
+
+Object.defineProperty(Person3.prototype,"constructor",{
+	enumerable : false
+})
+console.log(Object.keys(Person3.prototype));
+console.log(friend.constructor);
+
+Person3.prototype = {
+	test : "test1"
+}
+
+console.log(friend.test);	//重写原型对象切断了现有原型与任何之前已经存在的对象实例之间的联系
+
+
+
+
+//组合使用构造函数模式和原型模式
+function Person4(name,age,job) {
+	this.name = name;
+	this.age = age;
+	this.job = job;
+	this.friends = ["Shelby","Court"];
+}
+
+Person4.prototype = {
+	constructor : Person4,
+	sayName : function(){
+		console.log(this.name);
+	}
+}
+
+person1 = new Person4("Nicholas",20,"Software Engineer");
+person2 = new Person4("Greg",23,"Doctor");
+
+person1.friends.push("Van");
+
+console.log(person1);
+console.log(person2);
+person1.sayName();
+console.log(Person4.prototype);
+
+/**
+ * 动态原型模式
+ */
+function Person5(name,age,job){
+	this.name = name;
+	this.age = age;
+	this.job = job;
+	
+	//方法
+	if(typeof this.sayName != "function"){
+		Person5.prototype.sayName = function(){
+			console.log(this.name);
+		};
+	}
+}
+
+person = new Person5("Haha",35,"singer");
+person.sayName();
