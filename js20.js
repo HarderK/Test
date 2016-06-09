1f4750cadc6dc59e882ddbe4b7d9987960609088
Yanxiong <592917974@qq.com>
2016-06-09 20:00:12 +0800

    创建对象与继承复习

--- js20.js
+++ js20.js
@@ -24,3 +24,15 @@ console.log(bookCopy);
 console.log(typeof bookCopy);	//object
 console.log(typeof bookCopy.releaseDate);
 // JSON.parse(book1);
+
+//向现有URL的末尾添加查询字符串参数,每个参数的名称和值都必须使用encodeURIComponent()进行编码
+function addURLParam(url,name,value){
+	url += (url.indexof("?") == -1 ? "?" : "&");
+	url += encodeURIComponent(name) + "=" + encodeURIComponent(value);
+	return url;
+}
+
+xhr.open("post","postExample.php",true);
+xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");
+var form = document.getElementById("user-info");
+xhr.send(serialize(form));
--- test.js
+++ test.js
@@ -1,18 +1,32 @@
-var values = new Array();
-
-values.push.apply(values,[1,3,5]);
-
-console.log(values);
-
-function test(){
-	console.log(arguments);
-	console.log(arguments instanceof Array);
-	var arr = Array.prototype.slice.call(arguments);
-	console.log(arr);
-	console.log(arr instanceof Array);
+function SuperType(name){
+	this.name = name;
+	this.colors = ['red','blue'];
 }
-test(1,3,3,5);
+SuperType.prototype.sayName = function(){return this.name};
 
-var arr = [1,3,4,6,2];
-console.log(arr.slice());
+function SubType(name,age){
+	SuperType.call(this,name);
+	this.age = age;
+}
 
+SubType.prototype = (function(){
+	function F(){}
+	F.prototype = SuperType.prototype;
+	var proto = new F();
+	proto.constructor = SubType;
+	proto.sayAge = function(){return this.age;};	//增强对象
+	return proto;
+})();
+
+
+
+var instance = new SubType("James",28);
+console.log(instance.sayName());
+console.log(SubType.prototype);
+console.log(instance.sayAge());
+
+var arr = [];
+for (var property in instance){
+	arr.push(property);
+}
+console.log(arr);
