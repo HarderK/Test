d4156f61f94f463a2baad950e33c20716b0d9c49
yanxiong <592917974@qq.com>
2016-11-04 16:42:10 +0800

    -a

---
+++ ES6/1.js
@@ -0,0 +1,138 @@
+// 'use strict';
+var a = [];
+for(var i = 0; i < 10; i++) {
+	a[i] = function(){
+		console.log(i);
+	};
+}
+a[1]();
+
+var b = [];
+for (let j = 0; j < 10; j++) {
+	b[j] = function(){
+		console.log(j);
+	}
+}
+
+b[5]();
+
+
+/*{
+	console.log(dd);
+	let dd = 1;
+}*/
+
+//  暂时性死区
+var temp = 123;
+if(true) {
+	// temp = 'abc';
+	// typeof temp;
+	let temp;
+}
+
+function a() {
+	let a = 10;
+	// var a = 1;    let不允许在相同作用域内重复声明同一个变量
+}
+
+function func(arg) {	// 不能在函数内部重复声明参数
+	// let arg;
+}
+
+function func1 (arg) {		
+	{
+		let arg;
+	}
+}
+
+var tmp = new Date();
+
+function f() {
+	console.log(tmp);
+	if(false) {
+		var tmp = "hello world"
+	}
+}
+f();
+
+var s = 'hello'
+for (var i = 0; i < s.length; i++) {
+	console.log(s[i])
+}
+
+console.log(i)
+
+function f1() {
+	let n = 5;
+	{
+		let n = 10;
+	}
+	console.log(n);
+}
+
+f1();
+
+// ES6 引入了块级作用域，明确允许子啊块级作用域之中声明函数，声明语句的行为类似于let
+if(true) {
+	function f2() {}
+}
+
+function f3() {console.log('outside');}
+(function() {
+	if(false) {
+		let f3 = function() {console.log('inside');}
+	}
+	f3();
+}());
+
+
+// const声明一个只读的常量，一旦声明，常量的值就不能改变,只在声明所在的块级作用域内有效
+const PI = 3.1415;
+
+// 存在暂时性死区，不存在提升
+if(true) {
+	const MAX = 5;
+}
+// console.log(MAX)
+
+function* fibs() {
+	var a = 0, b = 1;
+	while (a < 10) {
+		yield a;
+		[a, b] = [b, a + b];
+	}
+}
+
+var [first, second, third, fourth, fifth, sixth] = fibs();
+console.log(first, second, third, fifth);
+
+// 解构赋值不仅可以用于数组还可以用于对象
+// 变量必须与属性同名才能取到正确的值
+var {bar, foo} = {foo: "aaa", bar:'bbb'};
+console.log(foo, bar);
+
+// 对象内部的解构赋值是先找到同名属性，然后再赋值给对应的变量
+var {foo: bar} = {foo:'aaa', ff: 'fff'};
+console.log(bar);
+
+// let命令的下一行 的圆括号是必须的，否则会报错。因为解析器首先会将起首的大括号理解成一个代码块而不是赋值语句。
+let kkk;f
+({kkk} = {kkk: 'abcd'});
+console.log(kkk);
+
+// 字符串的解构赋值
+const [A,B,c,d,e] = "hello";
+console.log(A,B,c,d,e);
+
+var arr = [[1,2], [3,4]];
+var arr1 = arr.map(([a,b]) => a+b);
+console.log(arr1);
+
+function move({x, y} = {x: 0, y: 0}) {
+	return [x, y];
+}
+console.log(move({}));
+
+var arr = [1, undefined, 3];
+console.log(arr.map((x='yes') => x));
+
---
+++ ES6/2.js
@@ -0,0 +1,109 @@
+function makeIterator(array) {
+	var nextIndex = 0;
+	return {
+		next: function() {
+			return nextIndex < array.length ? {value: array[nextIndex++], done: false} : {value: undefined, done: true};
+		}
+	}
+
+}
+
+var it = makeIterator(['a', 'b']);
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+
+
+var m = new Map();
+var o = {p: 'hello world'};
+m.set(o, 'content');
+console.log(m.get(o));
+m.delete(o);
+console.log(m.has(o));
+
+var items = [['name', '张三'], ['title', 'Author']];
+
+var map = new Map(items);
+console.log(map);
+
+function foo({x = 1, y = 5}) {
+	console.log(x, y);
+}
+foo({});
+
+/* 扩展运算符 */
+// 将一个数组转为用逗号分隔的参数序列
+console.log(...[1,2,3]);
+// 该运算符主要用于函数调用
+
+function f(x, y, z) {
+	return x + y + z;
+}
+
+console.log(f.apply(null, [ 1, 2, 3]));
+
+console.log(Math.max.apply(null, [14,3,77]));
+console.log(Math.max(...[14,3,77]));
+
+
+/* 扩展运算符的应用 */
+// 提供了数组合并的新写法
+{
+	let more = [1, 2, 3]
+	console.log([1, 2].concat(more));
+}
+
+// 与结构赋值结合
+{
+	const [first, ...rest] = [1,2,3,4,5];
+	console.log(rest);
+
+	// 如扩展运算符用于数组赋值，只能放在参数的最后一位，否则会报错
+	// const [...butLast, last] = [1, 2, 3, 4, 6];
+}
+
+// 扩展运算符可以将字符串转为真正的数组
+{
+	let arr = [...'hello world'];
+	console.log(arr);
+}
+
+// 任何实现了Iterator接口的对象，都可以通过扩展运算符转为真正的数组
+{
+/*	let nodeList = document.querySelectorAll('div');
+	let array = [... nodeList];*/
+}
+
+// 扩展运算符内部调用的是数据结构的Iterator接口，因此只要具有Iterator接口的对象都可以使用扩展运算符
+{
+	let map = new Map([
+		[1, 'one'],
+		[2, 'two'],
+		[3, 'three']
+		]);
+	console.log([...map.keys()]);
+
+	let go = function* () {
+		yield 1;
+		yield 2;
+		yield 3;
+	};
+	console.log([...go()]);
+}
+
+
+/* 箭头函数 */
+{
+	let f = v => v;
+	console.log(f(2));
+
+	f = () => 6;
+	console.log(f());
+	let sum = (num1, num2) => Math.min(num1, num2);
+	console.log(sum(2,33));
+
+	// 箭头函数的代码块多余一条语句，就要使用大括号将它们括起来,并且使用return语句返回
+	sum = (num1, num2) => { return num1 + num2;};
+	sum = (num1, num2) => ({id: num1, name: num2});
+	console.log(sum(2,3));
+}
