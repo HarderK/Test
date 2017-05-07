2ba08fc60b361e711746977b60927aaa2da7321e
yanxiong <yanxiong@yanxiongdeMacBook-Pro.local>
2017-05-07 20:51:06 +0800

    add

--- node.js/Cantelon/chapter8/photo/routes/photos.js
+++ node.js/Cantelon/chapter8/photo/routes/photos.js
@@ -1,5 +1,11 @@
 var express = require('express');
+var formidable = require('formidable');
+var join = require('path').join;
+var fs = require('fs');
+var Photo = require('../models/Photo');
+
 var router = express.Router();
+
 
 var photos = [];
 photos.push({
@@ -11,12 +17,65 @@ photos.push({
 	path: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1491976604218&di=4882f1dd9a81efaf9fe85dc0ff3e6688&imgtype=0&src=http%3A%2F%2Fimg3.redocn.com%2Ftupian%2F20151014%2Fkatongbeijingtupian_5086318_small.jpg'
 });
 
+// router.use()
 
 router.get('/', function(req, res, next) {
-	res.render('photos', {
-		title: 'Photos',
-		photos: photos
-	})
+	Photo.find({}, function(err, photos){
+		if(err) return next(err);
+		
+		res.render('photos', {
+			title: 'Photos',
+			photos: photos
+		});
+	});
+	
 });
 
+router.get('/upload', function(req, res, next) {
+	res.render('photos/upload', {
+		title: 'Photo upload'
+	});
+});
+
+router.get('/photo/:id/download', function(req, res, next) {
+	var id = req.params.id;
+	Photo.findById(id, function(err, photo) {
+		if(err) return next(err);
+		console.log(__dirname);
+		var path = join(req.app.get('photos'), photo.name);
+
+		//res.sendFile(path);		// 传输文件
+		res.download(path);
+	});
+});
+
+router.post('/upload', submit());
+
+function submit() {
+	return function(req, res, next) {
+		var type = req.headers['content-type'] || '';
+		if(type.indexOf('multipart/form-data') === 0) {
+  			var form = new formidable.IncomingForm();
+  			form.parse(req, function(err, fields, files){
+  				var path = join(req.app.get('photos'), fields['photo_name']);
+				
+
+				fs.rename(files['photo_image'].path, path, function(err) {
+					if(err) return next(err);
+
+					Photo.create({
+						name: fields['photo_name'],
+						path: join('/photos', fields['photo_name'])
+					}, function(err) {
+						if(err) return next(err);
+						// res.redirect('/');
+						res.redirect('/photos');
+					});
+				})
+
+  			});
+			
+		}
+	}
+}
 module.exports = router;
--- sort_search/quickSort2.js
+++ sort_search/quickSort2.js
@@ -5,7 +5,6 @@
 function quickSort(array, left, right) {
 	if(array.length < 2) return array;
 	var index = partition(array, left, right);
-	// console.log(array);
 	if(left < index - 1) quickSort(array, left, index - 1);
 	if(right > index + 1) quickSort(array, index + 1, right);
 }
@@ -14,7 +13,6 @@ var partition = function(array, left, right) {
 	var pivot = array[left],
 		i = left,
 		j = right;
-
 	while(i < j) {
 		while(i < j && array[j] >= pivot) {
 			j--;
@@ -22,14 +20,10 @@ var partition = function(array, left, right) {
 		while(i < j && array[i] <= pivot) {
 			i++;
 		}
-
 		if(i < j) {	
 			swapQuickSort(array, i, j);
 		}
-		console.log(array)
-
 	}
-
 	// 将基数归位
 	array[left] = array[i];
 	array[i] = pivot;
